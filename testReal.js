bb992deced1bb78de8948f47c7c0bcbe4465079c
Patricio Torres <patriciotorres@patricios-air.attlocal.net>
2017-08-27 13:45:13 -0700

    Eli bitching

--- testReal.js
+++ testReal.js
@@ -1,22 +1,30 @@
 _ = require("./underscoreMin");
 
-console.log(_.first([10, 1, 3]));
+//Test case for first(array, [n]);
+console.log("\nTest case for first(array, [n])");
+console.log(_.first([10, 1, 3, 5]));
+console.log(_.first([10, 1, 3, 5], 2));
+console.log(_.first([10, 1, 3, 5], 42));
 console.log(_.first(false));
 console.log(_.first([]));
 console.log(_.first(0));
 console.log(_.first({}));
 
-//intial() Test
-console.log(_.initial([10, 1, 3]));
-console.log(_.initial([10, 1, 3], 2));
+//Test case for initial(array, [n]);
+console.log("\nTest case for intial(array, [n])");
+console.log(_.initial([10, 1, 3, 5]));
+console.log(_.initial([10, 1, 3, 5], 2));
+console.log(_.initial([10, 1, 3, 5], 42));
 console.log(_.initial(false));
 console.log(_.initial([]));
 console.log(_.initial(0));
 console.log(_.initial({}));
 
-//last() Test
-console.log(_.last([10, 1, 3]));
-console.log(_.last([10, 1, 3], 2));
+//Test case for last(array, [n]);
+console.log("\nTest case for last(array, [n])");
+console.log(_.last([10, 1, 3, 5]));
+console.log(_.last([10, 1, 3, 5], 2));
+console.log(_.last([10, 1, 3, 5], 42));
 console.log(_.last(false));
 console.log(_.last([]));
 console.log(_.last(0));
